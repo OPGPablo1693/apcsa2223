81a8dbf7fc758de235e00762476956e36ad6f094
Chris <chsu5616@student.harmonytx.org>
2022-08-19 08:50:31 -0500

    This my First Comment

---
+++ MyFirstClass.java
@@ -0,0 +1,25 @@
+public class MyFirstClass
+{
+    // after you create your project and class
+    //you have to create your main method to run commands
+    //in order to use shortcut you type main and press <tab>
+    public static void main(String[] args)
+    {
+        // to print out something to the screen you type system.out.println("your text here");
+        //each line of the code must end with semicolon
+        //shortcut or system.out.println() is sout and <tab>
+        // this double // means this is a part of your code it is just a comment line to explain or taking note
+
+        // this is single line comment but is you need a multiple line
+        /*
+        this is multiple line comment until the end '
+        of
+        the comment
+        you can type your comment
+         */
+        System.out.println("My First Line of Code");
+        // another way to print on screen with System.out.print();
+        // the difference between the println the text and goes to the next line but print use the same line
+        //
+    }
+}
---
+++ varibles.java
@@ -0,0 +1,6 @@
+public class varibles {
+    public static void main(String[] args) {
+        // //Declaration of variables
+        //DateType
+    }
+}
