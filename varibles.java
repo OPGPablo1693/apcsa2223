5a92931642d69debbca38cfe0b80780bdc58cf52
Chris <chsu5616@student.harmonytx.org>
2022-08-19 09:14:37 -0500

    This my First Comment 08/19/2022

--- varibles.java
+++ varibles.java
@@ -1,6 +1,18 @@
+import com.sun.jdi.Value;
+
 public class varibles {
     public static void main(String[] args) {
         // //Declaration of variables
-        //DateType
+        //DateType variable = vaule;
+        String name = "Chris";
+        int myNum = 5;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+        byte smallNumber = 3;
+        short shortNumber = 4;
+        long longNumber = 123456789;
+        double doubleNumber = 12.34;
+        
     }
 }
