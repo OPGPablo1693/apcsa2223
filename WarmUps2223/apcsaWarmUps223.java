bfa4daaa9084a95f3c9a4f82bfb20c6ed6ced0a2
Chris <chsu5616@student.harmonytx.org>
2022-09-15 09:25:43 -0500

    Today is 08/25/22 REALATIONAL COMPARISON OPERATORS

---
+++ WarmUps2223/apcsaWarmUps223.java
@@ -0,0 +1,34 @@
+package WarmUps2223;
+
+import java.util.Scanner;
+
+public class apcsaWarmUps223
+{
+    public static void main(String[] args) {
+        // Redo the project Calculator with Switch Case instead of if logical statements.
+
+        System.out.println("Welcome to calculator Please select the operator");
+        Scanner scanner = new Scanner(System.in);
+        String operator = scanner.nextLine();
+
+        System.out.println("enter your two integer");
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        switch (operator)
+        {
+            case "+":
+                System.out.println("result = "+(number1+number2));
+                break;
+            case "-":
+                System.out.println("result = "+(number1-number2));
+                break;
+            case "/":
+                System.out.println("result = "+(number1/number2));
+                break;
+            case "*":
+                System.out.println("result = "+(number1*number2));
+                break;
+            default:
+        }
+    }
+}
